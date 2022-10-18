85bcf2655814a337a830c7140f60b352f2dfb508
Julisblake <89098405+Julisblake@users.noreply.github.com>
2022-10-18 14:32:53 -0300

    Update codigo_jogo_da_velha.js

--- codigo_jogo_da_velha.js
+++ codigo_jogo_da_velha.js
@@ -1,1 +1,2 @@
-var usuario1 = localStorage.getItem()
+document.getElementById('nome_usuario1').innerHTML = localStorage.getItem('Usuário1');
+document.getElementById('nome_usuario2').innerHTML = localStorage.getItem('Usuário2');
