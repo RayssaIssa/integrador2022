c0b807a4550ac6303074f4a05285a93b90207812
Julisblake <89098405+Julisblake@users.noreply.github.com>
2022-10-18 14:39:21 -0300

    Update codigo_pagina_inicial.js

--- codigo_pagina_inicial.js
+++ codigo_pagina_inicial.js
@@ -2,5 +2,5 @@ function submeter_nomes(){
   var nome_usuario1 = document.getElementById('user').value;
   localStorage.setItem('Usuário_1',nome_usuario1);
   var nome_usuario2 = socument.getElementById('user2').value;
-  localStorage.setItem('Usuário_1',nome_usuario2);
+  localStorage.setItem('Usuário_2',nome_usuario2);
 };
