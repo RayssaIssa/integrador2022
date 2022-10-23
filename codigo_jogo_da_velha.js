5b10f5f282f35583867e9e13201f932df818ad82
Julisblake <89098405+Julisblake@users.noreply.github.com>
2022-10-23 15:59:15 -0300

    Update codigo_jogo_da_velha.js

--- codigo_jogo_da_velha.js
+++ codigo_jogo_da_velha.js
@@ -2,7 +2,7 @@ jogador1 = [localStorage.getItem('user1_nome'), 6];
 jogador2 = [localStorage.getItem('user2_nome'), 6];
 
 document.getElementById('nome_usuario1').innerHTML = localStorage.getItem('user1_nome').toUpperCase() + "||| VIDAS: " + jogador1[1];
-document.getElementById('nome_usuario2').innerHTML = localStorage.getItem('user2_nome') + "||| VIDAS: " + jogador2[1];
+document.getElementById('nome_usuario2').innerHTML = localStorage.getItem('user2_nome').toUpperCase() + "||| VIDAS: " + jogador2[1];
 
 
 matriz_jogo = ['','','','','','','','',''];
