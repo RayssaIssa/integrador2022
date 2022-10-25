9c5c16188eab6a6743f284565d38ef86eddf7964
Julisblake <89098405+Julisblake@users.noreply.github.com>
2022-10-25 13:57:29 -0300

    Update codigo_pagina_vencedores.js

--- codigo_pagina_vencedores.js
+++ codigo_pagina_vencedores.js
@@ -1,7 +1,7 @@
 var ganhador = localStorage.getItem('Vencedor');
 
 if (ganhador == "Empate"){
-    document.getElementById('player') = "Deu velha!!!!! CORTEM-LHES AS CABEÇAS!!!!";
+    document.getElementById('player').innerHTML = "Deu velha!!!!! CORTEM-LHES AS CABEÇAS!!!!";
 }else{
   document.getElementById('player').innerHTML = "Parabéns " + ganhador + ", você foi o grande ganhador!!!!"
 };
