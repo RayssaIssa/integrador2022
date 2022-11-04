5833c6a8aeee67f8390dbe398eef3f4ca2fb1dc1
Julisblake <89098405+Julisblake@users.noreply.github.com>
2022-11-04 12:50:49 -0300

    Update codigo_pagina_inicial.js

--- codigo_pagina_inicial.js
+++ codigo_pagina_inicial.js
@@ -1,4 +1,4 @@
 function ativar(){
   localStorage.setItem('user1_nome', document.getElementById('user').value);
-  localStorage.setItem('user2_nome',document.getElementById('user2').value);
+  localStorage.setItem('user2_nome', document.getElementById('user2').value);
 };
